cf3abf792cd49b92856b3911a2ed9b71e4edc6a5
Florian Margaine <florian@platform.sh>
2017-09-15 10:56:31 +0200

    Fix pinentry.
    echo D $PID was broken.

--- util/pinentry/pinentry.lisp
+++ util/pinentry/pinentry.lisp
@@ -4,10 +4,10 @@
 (in-package #:pinentry)
 
 (defun main (stream)
-  (let ((password (stumpwm:read-one-line (stumpwm:current-screen)
-                                         "Passphrase: "
-                                         :password t)))
-    (format stream password)))
+  (format stream (or (stumpwm:read-one-line (stumpwm:current-screen)
+                                            "Passphrase: "
+                                            :password t)
+                     "")))
 
 (usocket:socket-server "127.0.0.1" 22222 #'main nil
                        :in-new-thread t
