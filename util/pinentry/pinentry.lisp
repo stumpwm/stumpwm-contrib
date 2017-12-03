f7db00147b5b486ecd7d5f8b739b0fc6cd012a34
Florian Margaine <florian@platform.sh>
2017-12-03 03:12:15 +0100

    multi-line prompts give new possibilities.

--- util/pinentry/pinentry.lisp
+++ util/pinentry/pinentry.lisp
@@ -4,10 +4,12 @@
 (in-package #:pinentry)
 
 (defun main (stream)
-  (format stream (or (stumpwm:read-one-line (stumpwm:current-screen)
-                                            "Passphrase: "
-                                            :password t)
-                     "")))
+  (let ((description (percent:decode (read-line stream)))
+        (prompt (read-line stream)))
+    (format stream (or (stumpwm:read-one-line (stumpwm:current-screen)
+                                              (format nil "~a~%~a " description prompt)
+                                              :password t)
+                       ""))))
 
 (usocket:socket-server "127.0.0.1" 22222 #'main nil
                        :in-new-thread t
