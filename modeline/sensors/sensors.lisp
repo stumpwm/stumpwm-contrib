fd6d23d0a042e8a3aa8b52a56bea995d9051069b
tslight <tslight@pm.com>
2019-06-07 12:47:40 +0100

    add vars for colors and remove bells and whistles of defcommand output
    - it makes the code too revolting!

--- modeline/sensors/sensors.lisp
+++ modeline/sensors/sensors.lisp
@@ -11,6 +11,15 @@
 (defvar *fan-regex* "(?<=\\:).*?(?=RPM)"
   "A regex that captures all fans.")
 
+(defvar *red-above* 60
+  "Temperature to turn red at.")
+
+(defvar *yellow-above* 50
+  "Temperature to turn yellow at.")
+
+(defvar *display-above* 40
+  "Temperature to start displaying at.")
+
 (defvar *ignore-below* 20
   "Ignore temperatures below this temperature when calculating average.")
 
@@ -24,44 +33,37 @@
 	     (list i))))
      strings)))
 
-(defun get-colors (value high mid low)
+(defun get-colors (value
+		   &optional
+		     (high *red-above*)
+		     (mid *yellow-above*)
+		     (low *display-above*))
   (cond ((< high value) (concat "^1*" (write-to-string value)))
 	((< mid value) (concat "^3*" (write-to-string value)))
 	((< low value) (write-to-string value))
 	(t nil)))
 
-(defun get-sensors (&optional as-string)
+(defun get-sensors ()
   (let* ((output (run-shell-command "sensors" t))
 	 (temps (sensors-as-ints output *temp-regex*))
 	 (fans (sensors-as-ints output *fan-regex*))
-	 (max-temp (get-colors (reduce #'max temps) 60 50 40))
+	 (max-temp (get-colors (reduce #'max temps)))
 	 (avg-temp (get-colors
 		    (handler-case
 			(floor (apply #'+ temps) (length temps))
-		      (division-by-zero () 0))
-		    60 50 40))
+		      (division-by-zero () 0))))
 	 (avg-rpm (get-colors
 		   (handler-case
 		       (floor (apply #'+ fans) (length fans))
 		     (division-by-zero () 0))
 		   4000 3000 2000)))
-    (if as-string
-	(concat
-	 (if max-temp (concat max-temp (string (code-char 176))) "C^n ")
-	 (if avg-temp (concat avg-temp (string (code-char 176))) "C^n ")
-	 (if avg-rpm (concat avg-temp "RPM^n")))
-	(list max-temp avg-temp avg-rpm))))
+    (concat
+     (if max-temp (concat max-temp (string (code-char 176)) "C^n"))
+     (if avg-temp (concat " " avg-temp (string (code-char 176)) "C^n"))
+     (if avg-rpm (concat " " avg-temp "RPM^n")))))
 
 (defcommand sensors () ()
-  (let* ((s (get-sensors))
-	 (max-temp (nth 0 s))
-	 (avg-temp (nth 1 s))
-	 (avg-rpm (nth 2 s)))
-    (message (concat
-	      "^B^5MAXIMUM TEMPERATURE: ^n~a" (string (code-char 176)) "C~%"
-	      "^B^5AVERAGE TEMPERATURE: ^n~a" (string (code-char 176)) "C~%"
-	      "^B^5AVERAGE FAN SPEED: ^n~a RPM")
-	     max-temp avg-temp avg-rpm)))
+  (message (get-sensors)))
 
 ;; pinched from battery portable code
 (let ((next 0)
@@ -72,6 +74,6 @@
       (when (< now next)
 	(return-from fmt-sensors last-value))
       (setf next (+ now *refresh-time*)))
-    (setf last-value (get-sensors t))))
+    (setf last-value (get-sensors))))
 
 (add-screen-mode-line-formatter #\S #'fmt-sensors)
