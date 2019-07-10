00a052d6f0dde3e607a2d14898679a0ec1c42bba
Brandon Invergo <bicom@invergo.net>
2019-07-10 22:14:03 +0100

    Add gnu-pw-mgr password-management utility

---
+++ util/gnu-pw-mgr/package.lisp
@@ -0,0 +1,7 @@
+;;;; package.lisp
+
+(defpackage #:gnu-pw-mgr
+  (:use #:cl :stumpwm)
+  (:export #:password-to-selection
+           #:*password-id-remember-timeout*
+           #:*clipboard-clear-timeout*))
