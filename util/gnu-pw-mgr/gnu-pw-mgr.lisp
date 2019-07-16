c654fd2e197f60b412a4a3acb68cf38200c0e44d
Brandon Invergo <bicom@invergo.net>
2019-07-16 23:26:37 +0100

    Add some comments and simplify the procedure a bit

--- util/gnu-pw-mgr/gnu-pw-mgr.lisp
+++ util/gnu-pw-mgr/gnu-pw-mgr.lisp
@@ -47,13 +47,19 @@
             (or (argument-pop-rest input)
                 (read-one-line (current-screen) prompt :password t)))))
 
-(defun parse-id-lines (lines)
+;; The tag lines look like:
+;; <seed tag>   <password>
+(defun parse-tags (lines)
   (loop :for index :from 1
-        :for id-line :in lines
-        :for id := (first (cl-ppcre:split " +" id-line))
-        :collect (format nil "~D) ~A" index id)))
+        :for tag-line :in lines
+        :collect (first (cl-ppcre:split " +" tag-line))))
 
-(defun label-line-p (line)
+;; Normally we could run gnu-pw-mgr with the '-H/--no-header' option
+;; to parse the output more easily, however we want to capture the
+;; login-id hint, which is omitted when -H is used.  So, we have to
+;; filter out a bunch of junk header lines when trying to isolate the
+;; seed-tag/password lines.
+(defun header-line-p (line)
   (cl-ppcre:scan "^$|^seed-tag|^login id hint:" line))
 
 (stumpwm:defcommand password-to-selection (pwid)
@@ -62,12 +68,16 @@
 the resulting password."
   (let* ((cmd (format nil "exec gnu-pw-mgr '~A'" pwid))
          (output (stumpwm:run-shell-command cmd t))
-         (lines (remove-if 'label-line-p (cl-ppcre:split "\\n" output)))
+         (lines (remove-if 'header-line-p (cl-ppcre:split "\\n" output)))
          (seed (select-from-menu (stumpwm:current-screen)
-                                 (parse-id-lines lines)
+                                 (parse-tags lines)
                                  "seed ID:"))
-         (seedno (when seed
-                     (parse-integer (first (cl-ppcre:split "\\) " seed))))))
+         (pass
+          (when seed
+            (second (cl-ppcre:split
+                     " +"
+                     (string (cl-ppcre:scan-to-strings
+                              (format nil "~A +\\S+" seed) output)))))))
     (unless *old-clipboard*
       (setf *old-clipboard* (get-x-selection)))
     (when (and *clipboard-clear-timeout*
@@ -77,8 +87,7 @@ the resulting password."
              (> *password-id-remember-timeout* 0))
         (reset-timer *password-id-timer* (* *password-id-remember-timeout* 60))
         (setf *password-id* nil))
-    (when seedno
-      (stumpwm:set-x-selection
-       (second (cl-ppcre:split " +" (nth (1- seedno) lines))))
+    (when pass
+      (stumpwm:set-x-selection pass)
       (alexandria:when-let ((string (cl-ppcre:scan-to-strings "login id hint: .+\\n" output)))
         (stumpwm:message string)))))
