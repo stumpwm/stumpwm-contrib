8b9f9e24fe3a1aaf7a1611b641655a1021d03382
cage <cage-invalid@invalid>
2019-12-29 10:58:18 +0100

    - using uninterned symbol in package definition.

--- modeline/disk/package.lisp
+++ modeline/disk/package.lisp
@@ -1,8 +1,8 @@
 ;;;; package.lisp
 
-(defpackage :disk
-  (:use :cl
-        :stumpwm)
+(defpackage #:disk
+  (:use #:cl
+        #:stumpwm)
   (:export
-   :*disk-modeline-fmt*
-   :*disk-usage-paths*))
+   #:*disk-modeline-fmt*
+   #:*disk-usage-paths*))
