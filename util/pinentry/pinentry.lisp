692a36f83d9ce699ec854c382a5b7800f6dae1d4
Spenser Truex <web@spensertruex.com>
2020-01-19 13:24:20 -0800

    Add timeout, fix "<character> is not bound" bug
    Implement pinentry's SETTIMEOUT command so there is enough time to enter a long
    message. The bug rendered StumpWM unusable.

--- util/pinentry/pinentry.lisp
+++ util/pinentry/pinentry.lisp
@@ -5,12 +5,17 @@
 
 (defun main (stream)
   (let ((description (percent:decode (read-line stream)))
-        (prompt (read-line stream)))
-    (format stream (or (stumpwm:read-one-line (stumpwm:current-screen)
-                                              (format nil "~a~%~a " description prompt)
-                                              :password t)
-                       ""))))
-
-(usocket:socket-server "127.0.0.1" 22222 #'main nil
-                       :in-new-thread t
-                       :multi-threading t)
+        (prompt (read-line stream))
+        (timeout (read-line stream)))
+    (handler-case
+        (usocket:socket-server "127.0.0.1" 22222 #'main nil
+                               :in-new-thread t
+                               :multi-threading t
+                               :timeout timeout)
+      (address-in-use-error (c) (declare (ignore c)))
+      (:no-error (c)
+        (declare (ignore c))
+        (format stream (or (stumpwm:read-one-line (stumpwm:current-screen)
+                                                           (format nil "~a~%~a " description prompt)
+                                                           :password t)
+                                    ""))))))
