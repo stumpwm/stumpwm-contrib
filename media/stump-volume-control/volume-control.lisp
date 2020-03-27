ab4d35454755446641d37c131003c9aa03e50f34
mgr <m.retzlaff@gmx.net>
2020-03-27 15:58:48 +0100

    allow to change the number of the sound card to control via amixer

--- media/stump-volume-control/volume-control.lisp
+++ media/stump-volume-control/volume-control.lisp
@@ -1,14 +1,18 @@
 (in-package :stump-volume-control)
 
+(defvar *sound-card* 0
+  "number of the sound card to control via amixer")
+
 (defcommand volume-up () ()
-  (run-shell-command "amixer -c 0 sset Master playback 2db+")
+  (run-shell-command (format nil "amixer -c ~d sset Master playback 2db+" *sound-card*))
   (message "Audio bit lowder."))
 
 (defcommand volume-down () ()
-  (run-shell-command "amixer -c 0 sset Master playback 2db-")
+  (run-shell-command (format nil "amixer -c ~d sset Master playback 2db-" *sound-card*))
   (message "Audio bit quieter."))
 
 (defcommand volume-toggle-mute () ()
   (let ((muted (search "[off]"
-		       (run-shell-command "amixer -c 0 sset Master playback toggle" t))))
+		       (run-shell-command (format nil "amixer -c ~d sset Master playback toggle" *sound-card*)
+                                          t))))
     (message (concatenate 'string "Audio " (if muted "muted" "back on") "."))))
