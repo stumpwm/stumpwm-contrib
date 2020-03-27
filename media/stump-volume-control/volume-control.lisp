4dc95a7418d0cf123837917db21c9bdc731664d3
mgr <m.retzlaff@gmx.net>
2020-03-27 19:22:57 +0100

    stump volume control: add unmute hack for PulseAudio
    (for details see comment in code)

--- media/stump-volume-control/volume-control.lisp
+++ media/stump-volume-control/volume-control.lisp
@@ -15,4 +15,9 @@
   (let ((muted (search "[off]"
                        (run-shell-command (format nil "amixer -c ~d sset Master playback toggle" *sound-card*)
                                           t))))
+    (unless muted
+      ;; hack for pulseaudio: on my system with PulseAudio muting Master also mutes Speaker
+      ;; but when unmuting Master Speaker stays muted, so unmuting it seperately.
+      ;; (On my system with just ALSA there is no Speaker so this will fail, but just silently...)
+      (run-shell-command (format nil "amixer -c ~d sset Speaker playback toggle" *sound-card*)))
     (message (concatenate 'string "Audio " (if muted "muted" "back on") "."))))
