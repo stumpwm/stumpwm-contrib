5c7db1843adfb55fcb0e8e1db435f504c6a429c1
mgr <m.retzlaff@gmx.net>
2020-03-30 16:06:10 +0200

    improve and better document the *pulse-audio-unmmute-hack* which also is controlled by this variable now

--- media/stump-volume-control/package.lisp
+++ media/stump-volume-control/package.lisp
@@ -2,4 +2,5 @@
 
 (defpackage #:stump-volume-control
   (:use #:cl #:stumpwm)
-  (:export #:volume-up #:volume-down #:volume-toggle-mute #:*sound-card*))
+  (:export #:volume-up #:volume-down #:volume-toggle-mute
+           #:*sound-card* #:*pulse-audio-unmute-hack*))
--- media/stump-volume-control/volume-control.lisp
+++ media/stump-volume-control/volume-control.lisp
@@ -3,6 +3,10 @@
 (defvar *sound-card* 0
   "number of the sound card to control via amixer")
 
+(defvar *pulse-audio-unmute-hack* nil
+  "PulseAudio mutes more than it unmutes; this hack will also unmute the outputs
+  (See comment in VOLUME-TOGGLE-MUTE for more details.)")
+
 (defcommand volume-up () ()
   (run-shell-command (format nil "amixer -c ~d sset Master playback 2db+" *sound-card*))
   (message "Audio bit lowder."))
@@ -15,9 +19,16 @@
   (let ((muted (search "[off]"
                        (run-shell-command (format nil "amixer -c ~d sset Master playback toggle" *sound-card*)
                                           t))))
-    (unless muted
-      ;; hack for pulseaudio: on my system with PulseAudio muting Master also mutes Speaker
-      ;; but when unmuting Master Speaker stays muted, so unmuting it seperately.
-      ;; (On my system with just ALSA there is no Speaker so this will fail, but just silently...)
-      (run-shell-command (format nil "amixer -c ~d sset Speaker playback toggle" *sound-card*)))
+    (when (and (not muted)
+               *pulse-audio-unmute-hack*)
+      ;; hack for pulseaudio:
+      ;;   On my system with PulseAudio muting 'Master' also mutes the
+      ;;   output 'Speaker' (or 'Headphone') but when unmuting 'Master'
+      ;;   the output 'Speaker (or 'Headphone') stays muted, so we just
+      ;;   explicitly unmute both seperately. This seems to work well.
+      ;;
+      ;;   As noted, this is a hack. It might work for you or it might not.
+      ;;   Of course, we cannot fix PulseAudio with a small hack here.
+      (run-shell-command (format nil "amixer -c ~d sset Speaker playback on" *sound-card*))
+      (run-shell-command (format nil "amixer -c ~d sset Headphone playback on" *sound-card*)))
     (message (concatenate 'string "Audio " (if muted "muted" "back on") "."))))
