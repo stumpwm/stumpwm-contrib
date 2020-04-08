726fdf4f5c42d05ba03fc510db29b478cc27e573
xstrtok <xstrtok@users.noreply.github.com>
2020-04-08 09:41:38 +0300

    Apply suggestions from code review
    Co-Authored-By: Javier Olaechea <pirata@gmail.com>

--- util/swm-ssh/swm-ssh.lisp
+++ util/swm-ssh/swm-ssh.lisp
@@ -2,38 +2,28 @@
 
 (defpackage #:swm-ssh
   (:use #:cl #:stumpwm)
-  (:export :*swm-ssh-default-term*
-           :*swm-ssh-config-path*)
+  (:export #:*swm-ssh-default-term*
+           #:*swm-ssh-config-path*)
   (:import-from #:cl-ppcre))
 
 (in-package #:swm-ssh)
 
 (defvar *swm-ssh-config-path* #p"~/.ssh/config")
 
-(defvar *swm-ssh-host-regex* "^Host")
+(defvar *swm-ssh-host-regex* "^Host[ \t]+")
 
 (defvar *swm-ssh-default-term* "urxvtc")
 
-(defun swm-ssh-read-config ()
-  (with-open-file (stream *swm-ssh-config-path* :direction :input)
+(defun collect-hosts (&optional (ssh-config *swm-ssh-config-path*))
+  (with-open-file (stream ssh-config :direction :input)
     (loop for line = (read-line stream nil)
-       while line
-       collect line)))
-
-(defun swm-ssh-collect-hosts ()
-  "Return a list of available hosts after stripping *swm-ssh-host-regex*"
-  (let ((ptrn (cl-ppcre:create-scanner *swm-ssh-host-regex*))
-        (host-list '(nil)))
-    (loop for line in (swm-ssh-read-config)
           while line
           when (cl-ppcre:scan *swm-ssh-host-regex* line)
             collect (cl-ppcre:regex-replace-all *swm-ssh-host-regex* line ""))))
 
 (stumpwm:defcommand swm-ssh-menu () ()
   "Select a host to ssh to"
-  (let ((entry (stumpwm:select-from-menu
-                (stumpwm:current-screen)
-                (mapcar 'list (swm-ssh-collect-hosts))
-                "Open ssh connection to: ")))
-    (when entry
-      (stumpwm:run-shell-command (format nil (concatenate 'string *swm-ssh-default-term* " -e ssh " (car entry)))))))
+  (alexandria:when-let ((entry (stumpwm:select-from-menu (stumpwm:current-screen)
+                                                         (mapcar 'list (swm-ssh-collect-hosts))
+                                "Open ssh connection to: ")))
+    (stumpwm:run-shell-command (format nil "~A -e ssh ~A" *swm-ssh-default-term* (car entry))))
