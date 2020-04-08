c3aa794e508a1b398e20c13669dbf2c009cc19a7
xstrtok <xstrtok@users.noreply.github.com>
2020-04-08 10:19:43 +0300

    Renamed variables from *swm-ssh-[term]* to *[term]
    Followed advice for rename, while kept *swm-ssh-default-term* and *swm-ssh-config-path*, as it's more easy to relate exported variables with package name.

--- util/swm-ssh/swm-ssh.lisp
+++ util/swm-ssh/swm-ssh.lisp
@@ -8,22 +8,24 @@
 
 (in-package #:swm-ssh)
 
-(defvar *swm-ssh-config-path* #p"~/.ssh/config")
+(defvar *config-path* #p"~/.ssh/config")
 
-(defvar *swm-ssh-host-regex* "^Host[ \t]+")
+(defvar *host-regex* "^Host[ \t]+")
 
 (defvar *swm-ssh-default-term* "urxvtc")
 
-(defun collect-hosts (&optional (ssh-config *swm-ssh-config-path*))
+(defun collect-hosts (&optional (ssh-config *config-path*))
   (with-open-file (stream ssh-config :direction :input)
     (loop for line = (read-line stream nil)
           while line
-          when (cl-ppcre:scan *swm-ssh-host-regex* line)
-            collect (cl-ppcre:regex-replace-all *swm-ssh-host-regex* line ""))))
+          when (cl-ppcre:scan *host-regex* line)
+            collect (cl-ppcre:regex-replace-all *host-regex* line ""))))
 
 (stumpwm:defcommand swm-ssh-menu () ()
   "Select a host to ssh to"
-  (alexandria:when-let ((entry (stumpwm:select-from-menu (stumpwm:current-screen)
-                                                         (mapcar 'list (swm-ssh-collect-hosts))
-                                "Open ssh connection to: ")))
-    (stumpwm:run-shell-command (format nil "~A -e ssh ~A" *swm-ssh-default-term* (car entry))))
+  (let ((entry (stumpwm:select-from-menu
+                (stumpwm:current-screen)
+                (mapcar 'list (collect-hosts))
+                "Open ssh connection to: ")))
+    (when entry
+      (stumpwm:run-shell-command (format nil "~A -e ssh ~A" *swm-ssh-default-term* (car entry))))))
