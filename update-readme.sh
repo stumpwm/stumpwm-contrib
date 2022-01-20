a2762328827202b060d519f0a2f0b22d65671b35
David <171410+dmb2@users.noreply.github.com>
2022-01-19 19:51:03 -0500

    Update update-readme.sh
    shell foo!

--- update-readme.sh
+++ update-readme.sh
@@ -4,13 +4,9 @@ sed '/# --/q' README.org > tmpReadme
 
 for f in $(find . -name "*.asd" | sort);
 do
-    if [[ -f "$(dirname $f)/README.md" ]]; then
-        README_PATH="README.md"
-    else
-        README_PATH="README.org"
-    fi
+    README_PATH=$(ls $(dirname $f)/README{,.txt,.md,.org} 2>/dev/null)
     desc=$(grep description $f | grep -o \".*\" | sed 's,",,g');
-    echo - [[$(dirname $f)/${README_PATH}][$(basename $f .asd)]] :: ${desc} >> tmpReadme
+    echo - [[${README_PATH}][$(basename $f .asd)]] :: ${desc} >> tmpReadme
 done
 awk '/\/util\// && !x {print "** Utilities"; x=1} 1' tmpReadme > tmp; mv tmp tmpReadme
 awk '/\/media\// && !x {print "** Media"; x=1} 1' tmpReadme > tmp; mv tmp tmpReadme
