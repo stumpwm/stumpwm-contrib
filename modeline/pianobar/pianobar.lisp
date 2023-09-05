efea53efc22365ad15e61eb5e5ec22f8b48adc83
Ahmed Khanzada <me@enzu.ru>
2023-09-05 19:38:11 -0400

    Minor fixes

--- modeline/pianobar/pianobar.lisp
+++ modeline/pianobar/pianobar.lisp
@@ -15,30 +15,13 @@
 
 (add-screen-mode-line-formatter #\P 'pianobar-modeline)
 
-;; Usage
-
-(defun pianobar-read-file-as-string (file-path)
-  (with-open-file (stream file-path :direction :input)
-    (let ((contents (make-string (file-length stream))))
-      (read-sequence contents stream)
-      contents)))
-
-(defun pianobar-now-playing ()
-  (let ((file-path "~/.config/pianobar/nowplaying"))
-    (pianobar-read-file-as-string file-path)))
-
-(defun fmt-pianobar-now-playing (now-playing)
-  "Returns Pianobar now playing info"
-  now-playing)
-
-(defun pianobar-modeline (ml)
-  (declare (ignore ml))
-  (format-expand *pianobar-formatters-alist*
-                 *pianobar-modeline-fmt*
-                 (pianobar-now-playing)))
+;; Variables
 
 (defvar *pianobar-formatters-alist*
   '((#\p  fmt-pianobar-now-playing)))
+
+(defvar *pianobar-now-playing-path*
+  "~/.config/pianobar/nowplaying")
 
 (defvar *pianobar-modeline-fmt* "%p"
   "The default value for displaying pianobar usage information on the modeline.
@@ -48,4 +31,27 @@
 A literal '%'
 @item %p
 Now playing
+@end table
 ")
+
+;; Functions
+
+(defun pianobar-read-file-as-string (file-path)
+  (with-open-file (stream file-path :direction :input)
+    (let ((contents (make-string (file-length stream))))
+      (read-sequence contents stream)
+      contents)))
+
+(defun pianobar-now-playing ()
+  (let ((file-path *pianobar-now-playing-path*))
+    (pianobar-read-file-as-string file-path)))
+
+(defun fmt-pianobar-now-playing (now-playing)
+  ;; May eventually do something more advanced than return its arg unscathed
+  "Returns Pianobar now playing info" now-playing)
+
+(defun pianobar-modeline (ml)
+  (declare (ignore ml))
+  (format-expand *pianobar-formatters-alist*
+                 *pianobar-modeline-fmt*
+                 (pianobar-now-playing)))
