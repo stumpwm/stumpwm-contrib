ac8445c0545dc94dde397ab69e3bbdc8514ae188
Kirill A. Korinsky <kirill@korins.ky>
2024-03-05 14:58:12 +0100

    percent-encode input password
    pinentry.texi cleary states that all string expected to be UTF-8 and if
    it contains special characters the string should be percent encoded.

--- util/pinentry/pinentry.lisp
+++ util/pinentry/pinentry.lisp
@@ -6,10 +6,12 @@
 (defun main (stream)
   (let ((description (percent:decode (read-line stream)))
         (prompt (read-line stream)))
-    (format stream (or (stumpwm:read-one-line (stumpwm:current-screen)
-                                              (format nil "~a~%~a " description prompt)
-                                              :password t)
-                       ""))))
+    (format stream
+            (percent:encode
+             (or (stumpwm:read-one-line (stumpwm:current-screen)
+                                               (format nil "~a~%~a " description prompt)
+                                               :password t)
+                        "")))))
 
 (handler-case (usocket:socket-server "127.0.0.1" 22222 #'main nil
                         :in-new-thread t
