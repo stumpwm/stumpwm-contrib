0250bbd4605453fb41eddd0632e94fb9e599486d
Laurent Cimon <laurent@nilio.ca>
2025-02-11 18:33:49 -0500

    stumptray: Adjust icon vertical and horizontal spacing

--- modeline/stumptray/package.lisp
+++ modeline/stumptray/package.lisp
@@ -5,6 +5,7 @@
   (:export *tray-viwin-background*
            *tray-hiwin-background*
            *tray-placeholder-pixels-per-space*
+	   *tray-icon-spacing*
 
            add-mode-line-hooks
            remove-mode-line-hooks
